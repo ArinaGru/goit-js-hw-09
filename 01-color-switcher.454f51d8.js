6f088cc4b502b39e0a6099a85f2f0fa0caff5a08
ArinaGru <98225650+ArinaGru@users.noreply.github.com>
2022-08-12 08:51:24 +0000

    Deploying to gh-pages from @ ArinaGru/goit-js-hw-09@89e2a90119b4288de7e014defdaf58c27a54c9b2 🚀

---
+++ 01-color-switcher.454f51d8.js
@@ -0,0 +1,2 @@
+const t={body:document.querySelector("body"),btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]")};t.btnStop.disabled=!0;let e=null;function n(){t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.btnStart.addEventListener("click",(function(){e=setInterval(n,1e3),t.btnStop.disabled=!1,t.btnStart.disabled=!0})),t.btnStop.addEventListener("click",(function(){clearInterval(e),t.btnStop.disabled=!0,t.btnStart.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.454f51d8.js.map
